ff87c290b771becad575b0ba10274f9818b3af65
Rex Jason Ordonez <rnordone@usc.edu>
2024-01-26 08:52:50 +0000

    Split

--- split.cpp
+++ split.cpp
@@ -11,13 +11,33 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
-
+#include <cstddef>
 /* Add a prototype for a helper function here if you need */
-
+void insertAtFront(Node*& head, Node* node) {
+    node->next = head;
+    head = node;
+}
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+if (in == NULL) {
+        return;
+    }
+
+    // Extract the first node from the input list
+    Node* currentNode = in;
+    in = in->next;
+
+    // Decide whether the node is odd or even and insert it to the appropriate list
+    if (currentNode->value % 2 == 0) {
+        insertAtFront(evens, currentNode);
+    } else {
+        insertAtFront(odds, currentNode);
+    }
+
+    // Recursive call
+    split(in, odds, evens);
 }
 
 /* If you needed a helper function, write it here */
